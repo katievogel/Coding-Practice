21ade8f2f0512aab0a47a907eb3bc134d048674f
Katie Vogel <kvog983@gmail.com>
2020-12-23 20:28:38 -0500

    more tree

--- binary-tree.js
+++ binary-tree.js
@@ -71,74 +71,122 @@ var tree = [4,
 // function mapTree (tree, f) {
 //     var [val, left, right] = tree;
 //     if (left !== null) {mapTree(left, f)};
-//     var newVal = f(val);
-//     if (right !== null) {mapTree(right, f)};
-//     return [newVal, left, right];
+// //     var newVal = f(val);
+// //     if (right !== null) {mapTree(right, f)};
+// //     return [newVal, left, right];
+// // }
+
+// // function mapTree(tree, f) {
+// //     var [val, left, right] = tree;
+// //     var newVal = f(val);
+// //     var newLeft = left !== null ? mapTree(left, f) : null;
+// //     var newRight = right !== null ? mapTree(right, f) : null;
+// //     return [newVal, newLeft, newRight];
+// // }
+
+// // var f = x => x + 2
+// // console.log(mapTree(tree, f));
+
+
+// // function mapTree2(tree, f) {
+// //     var [val, left, right] = tree;
+// //     var newVal = f(val);
+// //     var newLeft = left !== null ? mapTree2(left, f) : null;
+// //     var newRight = right !== null ? mapTree2(right, f) : null;
+// //     return [newVal, newRight, newLeft];
+// // }
+
+// // console.log(mapTree2(tree, f))
+
+// var tree2 = {
+//     'val': 4,
+//     'left': {
+//         'val': 2,
+//         'left': {
+//             'val': 1,
+//             'left': null,
+//             'right': null
+//         },
+//         'right': {
+//             'val': 3,
+//             'left': null,
+//             'right': null
+//         }
+//     },
+//     'right': {
+//         'val': 7,
+//         'left': {
+//             'val': 6,
+//             'left': null,
+//             'right': null
+//         },
+//         'right': {
+//             'val': 9,
+//             'left': null,
+//             'right': null
+//         }
+//     }
 // }
 
-// function mapTree(tree, f) {
-//     var [val, left, right] = tree;
-//     var newVal = f(val);
-//     var newLeft = left !== null ? mapTree(left, f) : null;
-//     var newRight = right !== null ? mapTree(right, f) : null;
-//     return [newVal, newLeft, newRight];
+// function invert(tree2) {
+//     //accounts for when a null root is passed in based on problem i did in leetcode
+//     if (tree2 === null) {
+//         return tree2
+//     }
+//     //let [val, left, right] = tree2;
+//     let { val, left, right } = tree2;
+//     left = left !== null ? invert(left) : null;
+//     right = right !== null ? invert(right) : null;
+//     return { val, left: right, right: left };
+//     //return [val, right, left];
 // }
 
-// var f = x => x + 2
-// console.log(mapTree(tree, f));
+// console.log(invert(tree2))
 
 
-// function mapTree2(tree, f) {
-//     var [val, left, right] = tree;
-//     var newVal = f(val);
-//     var newLeft = left !== null ? mapTree2(left, f) : null;
-//     var newRight = right !== null ? mapTree2(right, f) : null;
-//     return [newVal, newRight, newLeft];
-// }
 
-// console.log(mapTree2(tree, f))
+//Another binary tree problem, this one from leetcode. This time summing numbers from the tree within a low/high range. Should be noted that this would be more difficult to recreate in your IDE due to LeetCode abstracting away some tree info behind the scenes. I just c/p the whole problem for reference. The 'root' in question is not actually an array, even though it looks like that is supposed to be the input.
+ 
+//Definition for a binary tree node.
+//  function TreeNode(val, left, right) {
+//       this.val = (val===undefined ? 0 : val)
+//       this.left = (left===undefined ? null : left)
+//       this.right = (right===undefined ? null : right)
+//   }
 
-var tree2 = {
-    'val': 4,
-    'left': {
-        'val': 2,
-        'left': {
-            'val': 1,
-            'left': null,
-            'right': null
-        },
-        'right': {
-            'val': 3,
-            'left': null,
-            'right': null
-        }
-    },
-    'right': {
-        'val': 7,
-        'left': {
-            'val': 6,
-            'left': null,
-            'right': null
-        },
-        'right': {
-            'val': 9,
-            'left': null,
-            'right': null
-        }
+// /**
+//  * @param {TreeNode} root
+//  * @param {number} low
+//  * @param {number} high
+//  * @return {number}
+//  */
+// root = [10,5,15,3,7,null,18]
+// low = 7
+// high = 15
+//this solution below is slow as molasses, but it works. faster than only 5% of answers
+var rangeSumBST = function(root, low, high) {
+    var inRange = []
+    function traverseTree (root){
+        if (root.left !== null) traverseTree(root.left);
+        if (root.right !== null) traverseTree(root.right);
+        if (root.val >= low && root.val <= high){
+            inRange.push(root.val);
+        } 
+        var sumRange = inRange.reduce((acc, val) => acc + val, 0);
+        return sumRange;
     }
-}
-
-function invert(tree2){
-    //accounts for when a null root is passed in based on problem i did in leetcode
-    if (tree2 === null){
-        return tree2
+    return traverseTree(root);  
+};
+//trying to speed it up. this was fast than 55% of other answers. got rid of the array push and changed it to simple addition and got rid of the reduce. also move the return of of the summed numbers out so it's not constantly computing and throwing it away (wasteful)
+var rangeSumBST = function(root, low, high) {
+    var inRange = 0
+    function traverseTree (root){
+        if (root.left !== null) traverseTree(root.left);
+        if (root.right !== null) traverseTree(root.right);
+        if (root.val >= low && root.val <= high){
+            inRange += root.val;
+        } 
     }
-    //let [val, left, right] = tree2;
-    let {val, left, right} = tree2;
-    left = left !== null ? invert(left) : null;
-    right = right !== null ? invert(right) : null;
-    return {val, left: right, right: left};
-    //return [val, right, left];
-}
-
-console.log(invert(tree2))
+    traverseTree(root);
+    return inRange 
+};
